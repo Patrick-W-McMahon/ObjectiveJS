b3774821d006db487878b777a08619dbb22eafb2
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2014-03-25 09:16:35 -0400

    Update geo.o.js
    removed the R var that was not needed as we where using the this.currentRadius and so by removing the var R we will reduce the file size and the memory used.

--- geo.o.js
+++ geo.o.js
@@ -19,14 +19,13 @@ var GEO = {
 	}
 
 	distanceLatLonInKilometers: function(lat1,lon1,lat2,lon2){
-		var R = this.currentRadius;
 		var dLat = (lat2-lat1) * Math.PI / 180;
 		var dLon = (lon2-lon1) * Math.PI / 180;
 		var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
 			Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
 			Math.sin(dLon/2) * Math.sin(dLon/2);
 		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-		var d = R * c;
+		var d = this.currentRadius * c;
 		return d;
 	},
 
