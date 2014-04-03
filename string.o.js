d011581330a9d2a1e5c5774eb147b929b020c55b
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2014-04-03 11:58:15 -0400

    Update string.o.js
    replaced unset() with a delete so this object can run on by its self.

--- string.o.js
+++ string.o.js
@@ -2,7 +2,7 @@ String.strReplaceNthSub = function(needle, replacement, occurrence){
   var arr = this.split(needle);
   if(occurrence < arr.length){
     arr[occurrence - 1] += replacement + arr[occurrence];
-    arr[occurrence].unset();
+    delete arr[occurrence];
     this = arr.join();
   }
   return this;
