4bf9a53c878518cd56a03afb0719cfae6812e90a
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2014-06-21 13:34:00 -0400

    DOM.State() IE bug fix
    fixed compatibility with IE

--- dom.o.js
+++ dom.o.js
@@ -1,4 +1,4 @@
 function DOM(){
 
-	this.State = function(f){/in/(document.readyState)?setTimeout(this.State,9,f):f()};
+	this.State = function(f){/in/.test(document.readyState)?setTimeout('this.ready('+f+')',9):f()};
 };
