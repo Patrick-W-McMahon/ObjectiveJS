8d4a0635b1794d1ef4ea7ce73600400b77aeef5c
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2014-07-31 13:57:24 -0400

    now handles includes of other html documents.

--- dom_RenderEngine.js
+++ dom_RenderEngine.js
@@ -13,9 +13,44 @@
     if(elm.hasAttribute("href")&&elm.tagName.toLowerCase()!=="a"){
       elm.onclick = "javascript:location.href='"+elm.getAttribute("href")+"';return false";
     }
+    if(elm.tagName.toLowerCase()=="include"&&elm.hasAttribute("src")){
+      switch(elm.getAttribute("type").toLowerCase()){
+        case "javascript":
+          
+        break;
+        case "css":
+          
+        break;
+        default:
+        case "html":
+            var xmlhttp;
+            if (window.XMLHttpRequest) {
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            }else{
+                // code for IE6, IE5
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 ) {
+               if(xmlhttp.status == 200){
+                   document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+               }else if(xmlhttp.status == 400){
+                  alert('There was an error 400')
+               }else{
+                   alert('something else other than 200 was returned')
+               }
+            }
+            xmlhttp.open("GET", elm.getAttribute("src"), true);
+            var doc = xmlhttp.send();
+            var domElms = doc.body.childNodes;
+            for(var i=0; i<domElms.length; i++){
+              domElms[i].parentNode.insertBefore(elm, domElms[i]);
+            }
+            
+        break;
+      }
+    }
   }
   
 })();
-
-
-
