83b041829d11e9fbafbc993ac958e933576d09e2
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2014-07-31 14:28:43 -0400

    changed alert() to console.log()
    This way when errors happen its logged rather then popping up in the users face.

--- dom_RenderEngine.js
+++ dom_RenderEngine.js
@@ -36,9 +36,9 @@
                if(xmlhttp.status == 200){
                    document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
                }else if(xmlhttp.status == 400){
-                  alert('There was an error 400')
+                  console.log('There was an error 400')
                }else{
-                   alert('something else other than 200 was returned')
+                   console.log('something else other than 200 was returned')
                }
             }
             xmlhttp.open("GET", elm.getAttribute("src"), true);
