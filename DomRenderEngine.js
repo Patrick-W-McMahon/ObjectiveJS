dda00462dd3805b7b8de5b501274dbd53ec3a9d1
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2014-07-31 12:57:11 -0400

    Create DomRenderEngine.js
    A simple DOM Render Engine that adds new abilities to standard DOM elements.

---
+++ DomRenderEngine.js
@@ -0,0 +1,16 @@
+(function go() {
+  var childNodes = document.body.childNodes
+  for(var i=0; i<childNodes.length; i++) {
+    alert(childNodes[i])
+  }
+})();
+
+
+
+
+
+
+
+
+
+//onclick="javascript:location.href='http://www.uol.com.br/'"
