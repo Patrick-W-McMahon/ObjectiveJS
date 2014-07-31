d5421417225507a875c1d4977c3b18322c8f6d5b
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2014-07-31 13:11:36 -0400

    Update and rename DomRenderEngine.js to dom_RenderEngine.js
    any DOM item with an href is now clickable and will act like an 'a' tag

---
+++ dom_RenderEngine.js
@@ -0,0 +1,21 @@
+(function DOM_Render_Engine() {
+
+  (function traversDom(elm){
+    var childNodes = elm.childNodes;
+    for(var i=0; i<childNodes.length; i++) {
+      renderElm(childNodes[i]);
+      traversDom(childNodes[i]);
+      
+    }  
+  })(document.body);
+
+  function renderElm(elm){
+    if(elm.hasAttribute("href")&&elm.tagName.toLowerCase()!=="a"){
+      elemm.onclick = "javascript:location.href='"+elm.getAttribute("href")+"'";
+    }
+  }
+  
+})();
+
+
+
