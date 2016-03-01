2fd419535b2749b8cb429b2ca10c1eb226680d43
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2016-03-01 17:53:31 -0500

    Update array.o.js
    add move method to array lets index to new index.

--- array.o.js
+++ array.o.js
@@ -91,3 +91,13 @@ Array.prototype.clear = function(){
 	}
 	return 0;
 };
+
+Array.prototype.move = function (old_index, new_index) {
+	if (new_index >= this.length) {
+		var k = new_index - this.length;
+		while ((k--) + 1) {
+			this.push(undefined);
+		}
+	}
+	this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+};
